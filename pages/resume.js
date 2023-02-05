61e00b37fd59e98711bf72e20f201eeaa120f19a
Andrew Zang <andrewz020529@gmail.com>
2023-02-04 20:22:49 -0500

    kinda figured out how it works

--- pages/resume.js
+++ pages/resume.js
@@ -1,32 +1,32 @@
-import React, { useEffect, useState } from "react";
-import { useRouter } from "next/router";
-import Cursor from "../components/Cursor";
-import Header from "../components/Header";
-import ProjectResume from "../components/ProjectResume";
-import Socials from "../components/Socials";
-import Button from "../components/Button";
-import { useTheme } from "next-themes";
+import React, { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
+import Cursor from '../components/Cursor'
+import Header from '../components/Header'
+import ProjectResume from '../components/ProjectResume'
+import Socials from '../components/Socials'
+import Button from '../components/Button'
+import { useTheme } from 'next-themes'
 // Data
-import { name, showResume } from "../data/portfolio.json";
-import { resume } from "../data/portfolio.json";
-import data from "../data/portfolio.json";
+import { name, showResume } from '../data/portfolio.json'
+import { resume } from '../data/portfolio.json'
+import data from '../data/portfolio.json'
 
 const Resume = () => {
-  const router = useRouter();
-  const theme = useTheme();
-  const [mount, setMount] = useState(false);
+  const router = useRouter()
+  const theme = useTheme()
+  const [mount, setMount] = useState(false)
 
   useEffect(() => {
-    setMount(true);
+    setMount(true)
     if (!showResume) {
-      router.push("/");
+      router.push('/')
     }
-  }, []);
+  }, [])
   return (
     <>
-      {process.env.NODE_ENV === "development" && (
-        <div className="fixed bottom-6 right-6">
-          <Button onClick={() => router.push("/edit")} type={"primary"}>
+      {process.env.NODE_ENV === 'development' && (
+        <div className='fixed bottom-6 right-6'>
+          <Button onClick={() => router.push('/edit')} type={'primary'}>
             Edit Resume
           </Button>
         </div>
@@ -34,27 +34,37 @@ const Resume = () => {
       {data.showCursor && <Cursor />}
       <div
         className={`container mx-auto mb-10 ${
-          data.showCursor && "cursor-none"
+          data.showCursor && 'cursor-none'
         }`}
       >
         <Header isBlog />
         {mount && (
-          <div className="mt-10 w-full flex flex-col items-center">
+          <div className='mt-10 w-full flex flex-col items-center'>
             <div
               className={`w-full ${
-                mount && theme.theme === "dark" ? "bg-slate-800" : "bg-gray-50"
+                mount && theme.theme === 'dark' ? 'bg-slate-800' : 'bg-gray-50'
               } max-w-4xl p-20 mob:p-5 desktop:p-20 rounded-lg shadow-sm`}
             >
-              <h1 className="text-3xl font-bold">{name}</h1>
-              <h2 className="text-xl mt-5">{resume.tagline}</h2>
-              <h2 className="w-4/5 text-xl mt-5 opacity-50">
+              <div className='fixed up-6 right-6'>
+                <Button
+                  type={'primary'}
+                  onClick={() => {
+                    window.open('/')
+                  }}
+                >
+                  View PDF Version
+                </Button>
+              </div>
+              <h1 className='text-3xl font-bold'>{name}</h1>
+              <h2 className='text-xl mt-5'>{resume.tagline}</h2>
+              <h2 className='w-4/5 text-xl mt-5 opacity-50'>
                 {resume.description}
               </h2>
-              <div className="mt-2">
+              <div className='mt-2'>
                 <Socials />
               </div>
-              <div className="mt-5">
-                <h1 className="text-2xl font-bold">Experience</h1>
+              <div className='mt-5'>
+                <h1 className='text-2xl font-bold'>Experience</h1>
 
                 {resume.experiences.map(
                   ({ id, dates, type, position, bullets }) => (
@@ -68,27 +78,27 @@ const Resume = () => {
                   )
                 )}
               </div>
-              <div className="mt-5">
-                <h1 className="text-2xl font-bold">Education</h1>
-                <div className="mt-2">
-                  <h2 className="text-lg">{resume.education.universityName}</h2>
-                  <h3 className="text-sm opacity-75">
+              <div className='mt-5'>
+                <h1 className='text-2xl font-bold'>Education</h1>
+                <div className='mt-2'>
+                  <h2 className='text-lg'>{resume.education.universityName}</h2>
+                  <h3 className='text-sm opacity-75'>
                     {resume.education.universityDate}
                   </h3>
-                  <p className="text-sm mt-2 opacity-50">
+                  <p className='text-sm mt-2 opacity-50'>
                     {resume.education.universityPara}
                   </p>
                 </div>
               </div>
-              <div className="mt-5">
-                <h1 className="text-2xl font-bold">Skills</h1>
-                <div className="flex mob:flex-col desktop:flex-row justify-between">
+              <div className='mt-5'>
+                <h1 className='text-2xl font-bold'>Skills</h1>
+                <div className='flex mob:flex-col desktop:flex-row justify-between'>
                   {resume.languages && (
-                    <div className="mt-2 mob:mt-5">
-                      <h2 className="text-lg">Languages</h2>
-                      <ul className="list-disc">
+                    <div className='mt-2 mob:mt-5'>
+                      <h2 className='text-lg'>Languages</h2>
+                      <ul className='list-disc'>
                         {resume.languages.map((language, index) => (
-                          <li key={index} className="ml-5 py-2">
+                          <li key={index} className='ml-5 py-2'>
                             {language}
                           </li>
                         ))}
@@ -97,11 +107,11 @@ const Resume = () => {
                   )}
 
                   {resume.frameworks && (
-                    <div className="mt-2 mob:mt-5">
-                      <h2 className="text-lg">Frameworks</h2>
-                      <ul className="list-disc">
+                    <div className='mt-2 mob:mt-5'>
+                      <h2 className='text-lg'>Frameworks</h2>
+                      <ul className='list-disc'>
                         {resume.frameworks.map((framework, index) => (
-                          <li key={index} className="ml-5 py-2">
+                          <li key={index} className='ml-5 py-2'>
                             {framework}
                           </li>
                         ))}
@@ -110,11 +120,11 @@ const Resume = () => {
                   )}
 
                   {resume.others && (
-                    <div className="mt-2 mob:mt-5">
-                      <h2 className="text-lg">Others</h2>
-                      <ul className="list-disc">
+                    <div className='mt-2 mob:mt-5'>
+                      <h2 className='text-lg'>Others</h2>
+                      <ul className='list-disc'>
                         {resume.others.map((other, index) => (
-                          <li key={index} className="ml-5 py-2">
+                          <li key={index} className='ml-5 py-2'>
                             {other}
                           </li>
                         ))}
@@ -128,7 +138,7 @@ const Resume = () => {
         )}
       </div>
     </>
-  );
-};
+  )
+}
 
-export default Resume;
+export default Resume
