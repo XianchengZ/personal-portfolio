921f36a1bef2990a5abf86efc073057f6df543ca
Andrew Zang <andrewz020529@gmail.com>
2023-02-12 13:47:16 -0500

    rename projects

--- README.md
+++ README.md
@@ -3,5 +3,3 @@
 This repo is currently under development.
 
 This project leverages NextJS framework.
-
-Thanks to [Chetan Verma](https://github.com/chetanverma16) for the beautiful template.
--- pages/api/projects/edit.js
+++ pages/api/projects/edit.js
@@ -1,14 +1,14 @@
-import fs from "fs";
-import { join } from "path";
-import matter from "gray-matter";
+import fs from 'fs'
+import { join } from 'path'
+import matter from 'gray-matter'
 
 export default function handler(req, res) {
-  const postsfolder = join(process.cwd(), `/_posts/`);
-  if (process.env.NODE_ENV === "development") {
-    if (req.method === "POST") {
-      const { date, title, tagline, preview, image } = req.body.variables;
+  const postsfolder = join(process.cwd(), `/projects/`)
+  if (process.env.NODE_ENV === 'development') {
+    if (req.method === 'POST') {
+      const { date, title, tagline, preview, image } = req.body.variables
       fs.writeFile(
-        postsfolder + req.body.slug + ".md",
+        postsfolder + req.body.slug + '.md',
         matter.stringify(req.body.content, {
           date,
           title,
@@ -16,14 +16,14 @@ export default function handler(req, res) {
           preview,
           image,
         }),
-        "utf-8",
+        'utf-8',
         (err) => console.log(err)
-      );
-      res.status(200).json({ status: "DONE" });
+      )
+      res.status(200).json({ status: 'DONE' })
     } else {
       res
         .status(200)
-        .json({ name: "This route works in development mode only" });
+        .json({ name: 'This route works in development mode only' })
     }
   }
 }
