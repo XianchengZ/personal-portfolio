af47ab8ac00c99ab612b4ef05935c0d7b72bea2d
Andrew Zang <andrewz020529@gmail.com>
2023-02-13 15:05:43 -0500

    updated skills rough

--- components/Skills/index.js
+++ components/Skills/index.js
@@ -42,24 +42,74 @@ const products = [
   // More products...
 ]
 
+// import typescript from 'images/typescript.png'
+
+const skillsData = {
+  Frameworks: [],
+  'Technologies/Tools': [],
+}
+const Languages = [
+  {
+    id: 1,
+    name: 'Typescript',
+    imageSrc: '/skills/typescript.png',
+    href: '#',
+  },
+  {
+    id: 2,
+    name: 'JavaScript',
+    imageSrc: '/skills/javascript.png',
+    href: '#',
+  },
+  {
+    id: 3,
+    name: 'C++',
+    imageSrc: '/skills/c++_logo.png',
+    href: '#',
+  },
+  // {
+  //   id: 1,
+  //   name: 'Typescript',
+  //   imageSrc: '/skills/typescript.png',
+  //   href: '#',
+  // },
+]
+
+// products.map((product) => (
+//   <a key={product.id} href={product.href} className='group'>
+//     <div className='aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8'>
+//       {/* <img
+//         src={product.imageSrc}
+//         alt={product.imageAlt}
+//         className='h-full w-full object-cover object-center group-hover:opacity-75'
+//       /> */}
+//       <img
+//         src={'/skills/typescript.png'}
+//         alt={'typescript'}
+//         className='h-full w-full object-cover object-center group-hover:opacity-75'
+//       />
+//     </div>
+//   </a>
+// ))
+
 export default function Skills() {
   return (
     // <div className='bg-white'>
     <div className='mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8'>
       <div className='grid grid-cols-5 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8'>
-        {products.map((product) => (
-          <a key={product.id} href={product.href} className='group'>
+        {Languages.map((language) => (
+          <a key={language.id} href={language.href} className='group'>
             <div className='aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8'>
               <img
-                src={product.imageSrc}
-                alt={product.imageAlt}
-                className='h-full w-full object-cover object-center group-hover:opacity-75'
+                src={language.imageSrc}
+                alt={language.name}
+                className='h-30 w-30 object-cover object-center group-hover:opacity-75'
               />
+              <p>{language.name}</p>
             </div>
           </a>
         ))}
       </div>
     </div>
-    // </div>
   )
 }
